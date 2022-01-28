b269168e50b23680d631b873464ae7f49e752aee
david de la puente enriquez <Twinkymen@hotmail.com>
2022-01-28 15:32:43 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+
+//    public static void main(String[] args) {
+
+            public static void main(String[] args) {
+
+                Scanner input = new Scanner(System.in);
+
+                System.out.println("Nom1:");
+                String Nom1 = input.next();
+
+                System.out.println("Nom2:");
+                String Nom2 = input.next();
+
+                System.out.println();
+                System.out.println("resultat:" + Nom1 + Nom2);
+
+            }
+        }
+
+
